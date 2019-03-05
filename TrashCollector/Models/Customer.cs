6e914cdb8e6aa2bdbe3602ef7d249a9c383dd275
matthew payne <mdpayne26@yahoo.com>
2019-03-05 11:21:43 -0600

    i just added all the properties to the customer class

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer : User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ExtraDay {get; set;}
+        public int Balance { get; set; }
+        public string SuspendStartDate { get; set; }
+        public string SuspendEndDAte { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
+        public int UserRolesId { get; set; }
+        public string PickupDay { get; set; }
+    }
+}
